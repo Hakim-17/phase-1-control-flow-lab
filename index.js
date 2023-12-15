5a9b8950a21ed3c0b4bb24cd38438efc24a23a0e
Abdulhakim <abdulhakim.abdalla@student.moringaschool.com>
2023-12-15 15:10:11 +0300

    First Commit

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance<=400) {
+    return 'This one is on me!';
+  }
+    else if (distance>2000 && distance<2501) {
+      return 'I will gladly take your thirty bucks.';
+    } 
+    else if (distance>2500){
+      return 'No can do.';
+    }
 }
+console.log(scuberGreetingForFeet(1));
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city == 'NYC') {
+    return 'Ok, sounds good.';
+  } 
+ else if(city!='NYC'){
+  return 'No go.';
+ }
 }
+console.log(ternaryCheckCity('NYC'));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip=='generous') {
+    return 'Thank you so much.';
+  }
+  else if (tip !== 'generous'){
+    return 'Thank you.';
+  }
+ else {
+    return 'Bye.';
+  }
 }
+//console.log(switchOnCharmFromTip('generous'));
